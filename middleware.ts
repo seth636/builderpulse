d5cd0b83bf627340990e2e73f28e54c9b3e48252
Seth T Van Daele <seth@Seths-Mac-mini.local>
2026-03-28 04:43:18 -0700

    fix: exclude /api/cron from auth middleware

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,6 @@ export { default } from 'next-auth/middleware';
 
 export const config = {
   matcher: [
-    '/((?!api/auth|login|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api/auth|api/cron|login|_next/static|_next/image|favicon.ico).*)',
   ],
 };
